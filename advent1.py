2db8a130baba7380d55ec5439f59da7f81670d37
Melissa Blades <melissa@saltymermaid.dev>
2023-12-01 06:07:50 -0500

    dry

--- advent1.py
+++ advent1.py
@@ -11,14 +11,9 @@ def is_number(n):
     return is_num
 
 
-def get_first_num(cal_val):
-    for char in cal_val:
-        if is_number(char):
-            return int(char)
-
-
-def get_last_num(cal_val):
-    for char in reversed(cal_val):
+def get_num(cal_val, direction):
+    values = cal_val[::direction]
+    for char in values:
         if is_number(char):
             return int(char)
 
@@ -27,8 +22,8 @@ if __name__ == "__main__":
     total = 0
 
     for cv in test_data:
-        first_num = get_first_num(cv)
-        last_num = get_last_num(cv)
+        first_num = get_num(cv, 1)
+        last_num = get_num(cv, -1)
         total += ((first_num * 10) + last_num)
 
     print(total)
